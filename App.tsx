4be29c0a039be0fed51e995f468a22c0a795d38d
chris.lee@myhomevault.com.au <chris.lee@myhomevault.com.au>
2022-03-31 18:04:02 +1100

    Test Expo Notification

--- App.tsx
+++ App.tsx
@@ -1,10 +1,20 @@
+import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Notification from 'expo-notifications';
 
 export default function App() {
+  useEffect(() => {
+    registerForPushNotification().then(token => console.log(token)).catch(err => console.log(err));
+  })
+  async function registerForPushNotification() {
+    // Get the token that uniquely identifies this device
+    return (await Notification.getExpoPushTokenAsync()).data;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>TEST PUSH NOTIFICATION</Text>
       <StatusBar style="auto" />
     </View>
   );
